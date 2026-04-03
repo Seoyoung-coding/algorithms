50eceb20a63c8d2eaaac727e7bbe06947e9dfef4
seoyoung <seoyoung.project@gmail.com>
2026-04-04 00:42:03 +0900

    non_duplicate_example

---
+++ two_pointers/target_sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, arr, target_sum):
+        left, right = 0, len(arr)-1
+        while(left < right):
+            current_sum = arr[left] + arr[right]
+            if current_sum == target_sum:
+                return [left, right]
+
+            if target_sum > current_sum:
+                left += 1
+            else:
+                right -= 1
+        return [-1, -1]
+    
+def main():
+    sol = Solution();
+    print(sol.search([1, 2, 3, 4, 6], 6))
+    print(sol.search([2, 5, 9, 11], 11))
