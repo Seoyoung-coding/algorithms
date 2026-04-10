df32fa2ec568e82f036ba24199244134fd32fe65
seoyoung <seoyoung.project@gmail.com>
2026-04-10 21:48:43 +0900

    triplet 2 example solution

---
+++ two_pointers/triplet_target.py
@@ -0,0 +1,35 @@
+import math
+
+class Solution:
+    def searchTriplet(self, arr, target_sum):
+        arr.sort()
+        smallest_difference = math.inf
+        for i in range(len(arr)-2):
+            left = i + 1
+            right = len(arr) - 1
+            while (left < right):
+                target_diff = target_sum - arr[i] - arr[left] - arr[right]
+                if target_diff == 0:
+                    return target_sum
+
+                if abs(target_diff) < abs(smallest_difference) \
+                        or (abs(target_diff) == abs(smallest_difference) and target_diff > smallest_difference):
+                    smallest_difference = target_diff
+
+                if target_diff > 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return target_sum - smallest_difference
+
+
+def main():
+    sol = Solution()
+    print(sol.searchTriplet([-1, 0, 2, 3], 2))
+    print(sol.searchTriplet([-3, -1, 1, 2], 1))
+    print(sol.searchTriplet([1, 0, 1, 1], 100))
+    print(sol.searchTriplet([0, 0, 1, 1, 2, 6], 5))
+
+
+main()
